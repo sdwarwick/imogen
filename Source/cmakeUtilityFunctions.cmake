78b8a97f8c46e599257c321c4c8234a845e3fffe
Ben Vining <ben.the.vining@gmail.com>
2021-02-12 16:45:29 -0700

    Update cmakeUtilityFunctions.cmake

--- Source/cmakeUtilityFunctions.cmake
+++ Source/cmakeUtilityFunctions.cmake
@@ -9,23 +9,23 @@
 
 function (configureStandaloneExecutable)
 
-	if (NOT isBuildingStandalone AND NOT IMOGEN_launchStandaloneOnBuild AND NOT IMOGEN_preferStandaloneForAllTarget)  # nothing we're doing with the current build requires the standalone executable at all...
-	    set (IMOGEN_launchStandaloneOnBuild FALSE PARENT_SCOPE)
-	    return()
-	endif()
+    if (NOT isBuildingStandalone AND NOT IMOGEN_launchStandaloneOnBuild AND NOT IMOGEN_preferStandaloneForAllTarget)  # nothing we're doing with the current build requires the standalone executable at all...
+        set (IMOGEN_launchStandaloneOnBuild FALSE PARENT_SCOPE)
+        return()
+    endif()
 
-	#
+    #
 
-	message (STATUS "Configuring Imogen Standalone executable...")
+    message (STATUS "Configuring Imogen Standalone executable...")
 
-	#
-	function (_standaloneNotFound)
-		message (WARNING "Standalone executable not found, and Standalone is not a current build target. Auto-launch feature disabled.")
-	    set (IMOGEN_launchStandaloneOnBuild FALSE PARENT_SCOPE)
-	endfunction()
-	#
+    #
+    function (_standaloneNotFound)
+    	message (WARNING "Standalone executable not found, and Standalone is not a current build target. Auto-launch feature disabled.")
+        set (IMOGEN_launchStandaloneOnBuild FALSE PARENT_SCOPE)
+    endfunction()
+    #
 
-	set (standalonePath ${CMAKE_CURRENT_LIST_DIR}/Builds/Imogen_artefacts/Debug/Standalone)
+    set (standalonePath ${CMAKE_CURRENT_LIST_DIR}/Builds/Imogen_artefacts/Debug/Standalone)
 
     if (APPLE)
         set (standalonePath ${standalonePath}/Imogen.app)
@@ -37,23 +37,23 @@ function (configureStandaloneExecutable)
 
     #
 
-	if (NOT ( DEFINED IMOGEN_standalone_exec_path AND EXISTS ${IMOGEN_standalone_exec_path} ))  # maybe the user has supplied a valid path to the executable somewhere else on their system?
-	    if (isBuildingStandalone OR EXISTS ${standalonePath})
-	    	set (IMOGEN_standalone_exec_path ${standalonePath} CACHE FILEPATH "Path to the Imogen standalone executable file" FORCE)
-	    else()
-	    	_standaloneNotFound()
-	    	return()
-	    endif()
-	endif()
+    if (NOT ( DEFINED IMOGEN_standalone_exec_path AND EXISTS ${IMOGEN_standalone_exec_path} ))  # maybe the user has supplied a valid path to the executable somewhere else on their system?
+        if (isBuildingStandalone OR EXISTS ${standalonePath})
+        	set (IMOGEN_standalone_exec_path ${standalonePath} CACHE FILEPATH "Path to the Imogen standalone executable file" FORCE)
+        else()
+        	_standaloneNotFound()
+        	return()
+        endif()
+    endif()
 
-	#
+    #
 
-	if (NOT ( isBuildingStandalone OR EXISTS ${IMOGEN_standalone_exec_path} ))
-		_standaloneNotFound()
-		return()
-	endif()
+    if (NOT ( isBuildingStandalone OR EXISTS ${IMOGEN_standalone_exec_path} ))
+    	_standaloneNotFound()
+    	return()
+    endif()
 
-	set (IMOGEN_launchStandaloneOnBuild TRUE PARENT_SCOPE)
+    set (IMOGEN_launchStandaloneOnBuild TRUE PARENT_SCOPE)
 
     if (NOT isBuildingStandalone)
         message (WARNING "The Standalone executable was located and can be used, but you are not rebuilding the Standalone with this build, so its behavior may not reflect the most recent code changes.")
@@ -69,13 +69,13 @@ endfunction()
 
 function (configureAudioPluginHostExecutable)
 
-	message (STATUS "Configuring JUCE AudioPluginHost executable...")
+    message (STATUS "Configuring JUCE AudioPluginHost executable...")
 
-	if (DEFINED IMOGEN_AudioPluginHost_Path AND EXISTS ${IMOGEN_AudioPluginHost_Path}) # maybe the user has supplied a valid path to the executable already?
-		return()
-	endif()
+    if (DEFINED IMOGEN_AudioPluginHost_Path AND EXISTS ${IMOGEN_AudioPluginHost_Path}) # maybe the user has supplied a valid path to the executable already?
+    	return()
+    endif()
 
-	set (pluginHostPath ${IMOGEN_juceDir}/extras/AudioPluginHost/Builds)
+    set (pluginHostPath ${IMOGEN_juceDir}/extras/AudioPluginHost/Builds)
 
     if (APPLE)
         set (pluginHostPath ${pluginHostPath}/MacOSX/build/Debug/AudioPluginHost.app)
@@ -85,14 +85,14 @@ function (configureAudioPluginHostExecutable)
         set (pluginHostPath ${pluginHostPath}/VisualStudio2019/build/Debug/AudioPluginHost.exe)
     endif()
 
-	if (EXISTS ${pluginHostPath})
-		set (IMOGEN_AudioPluginHost_Path ${pluginHostPath} CACHE FILEPATH "Path to the JUCE AudioPluginHost executable" FORCE)
-		return()
-	endif()
+    if (EXISTS ${pluginHostPath})
+    	set (IMOGEN_AudioPluginHost_Path ${pluginHostPath} CACHE FILEPATH "Path to the JUCE AudioPluginHost executable" FORCE)
+    	return()
+    endif()
 
-	# TO DO: automatically build the APH here...?
+    # TO DO: automatically build the APH here...?
 
-	message (WARNING "AudioPluginHost executable could not be found; auto-launch feature disabled")
+    message (WARNING "AudioPluginHost executable could not be found; auto-launch feature disabled")
     set (IMOGEN_launchAudioPluginHostOnBuild FALSE PARENT_SCOPE)
 
 endfunction()
@@ -103,48 +103,48 @@ endfunction()
 
 function (configureIndividualBuildTargets)
 
-	if (IMOGEN_launchAudioPluginHostOnBuild OR IMOGEN_launchStandaloneOnBuild)
-	    set (autoLaunchingAnything TRUE)
+    if (IMOGEN_launchAudioPluginHostOnBuild OR IMOGEN_launchStandaloneOnBuild)
+        set (autoLaunchingAnything TRUE)
 
-	    if (IMOGEN_preferStandaloneForAllTarget OR NOT IMOGEN_launchAudioPluginHostOnBuild)
-			set (useStandaloneForAllTarget TRUE)
-		else()
-			set (useStandaloneForAllTarget FALSE)
-		endif()
-	else()
-	    set (autoLaunchingAnything FALSE)
-	endif()
+        if (IMOGEN_preferStandaloneForAllTarget OR NOT IMOGEN_launchAudioPluginHostOnBuild)
+    		set (useStandaloneForAllTarget TRUE)
+    	else()
+    		set (useStandaloneForAllTarget FALSE)
+    	endif()
+    else()
+        set (autoLaunchingAnything FALSE)
+    endif()
 
-	#
+    #
 
-	foreach (target ${formats} "All") 
-	    set (thisTargetName "Imogen_${target}")  # this is how JUCE automatically names the build targets created for each format
+    foreach (target ${formats} "All") 
+        set (thisTargetName "Imogen_${target}")  # this is how JUCE automatically names the build targets created for each format
 
-	    if (NOT TARGET ${thisTargetName})
-	        continue()
-	    endif()
+        if (NOT TARGET ${thisTargetName})
+            continue()
+        endif()
 
-	    set_target_properties (${thisTargetName} PROPERTIES FOLDER "Build Targets" XCODE_GENERATE_SCHEME ON)
+        set_target_properties (${thisTargetName} PROPERTIES FOLDER "Build Targets" XCODE_GENERATE_SCHEME ON)
 
-	    if (NOT autoLaunchingAnything)
-	        continue()
-	    endif()
+        if (NOT autoLaunchingAnything)
+            continue()
+        endif()
 
-	    if (IMOGEN_launchStandaloneOnBuild)
-	        if ("${target}" STREQUAL "Standalone" OR ("${target}" STREQUAL "All" AND useStandaloneForAllTarget))
-	            set_target_properties (${thisTargetName} PROPERTIES XCODE_SCHEME_EXECUTABLE ${IMOGEN_standalone_exec_path})
-	            message (STATUS "Executable for ${thisTargetName} set to 'Standalone'")
-	            continue()
-	        endif()
-	    elseif ("${target}" STREQUAL "Standalone")
-	        continue()
-	    endif()
+        if (IMOGEN_launchStandaloneOnBuild)
+            if ("${target}" STREQUAL "Standalone" OR ("${target}" STREQUAL "All" AND useStandaloneForAllTarget))
+                set_target_properties (${thisTargetName} PROPERTIES XCODE_SCHEME_EXECUTABLE ${IMOGEN_standalone_exec_path})
+                message (STATUS "Executable for ${thisTargetName} set to 'Standalone'")
+                continue()
+            endif()
+        elseif ("${target}" STREQUAL "Standalone")
+            continue()
+        endif()
 
-	    if (IMOGEN_launchAudioPluginHostOnBuild)
-	    	set_target_properties (${thisTargetName} PROPERTIES XCODE_SCHEME_EXECUTABLE ${IMOGEN_AudioPluginHost_Path})
-	        message (STATUS "Executable for ${thisTargetName} set to 'AudioPluginHost'")
-	    endif()
-	endforeach()
+        if (IMOGEN_launchAudioPluginHostOnBuild)
+        	set_target_properties (${thisTargetName} PROPERTIES XCODE_SCHEME_EXECUTABLE ${IMOGEN_AudioPluginHost_Path})
+            message (STATUS "Executable for ${thisTargetName} set to 'AudioPluginHost'")
+        endif()
+    endforeach()
 
 endfunction()
 
@@ -154,36 +154,36 @@ endfunction()
 
 function (checkAllDirectories)
 
-	if (NOT EXISTS ${sourceDir})
-	    message (FATAL_ERROR "Source code folder not found")
-	elseif (NOT EXISTS ${dspModulesPath})
-	    message (FATAL_ERROR "DSP modules folder not found")
-	elseif (NOT EXISTS ${GraphicAssetsDir})
-	    message (FATAL_ERROR "Graphic assets folder not found")
-	endif()
+    if (NOT EXISTS ${sourceDir})
+        message (FATAL_ERROR "Source code folder not found")
+    elseif (NOT EXISTS ${dspModulesPath})
+        message (FATAL_ERROR "DSP modules folder not found")
+    elseif (NOT EXISTS ${GraphicAssetsDir})
+        message (FATAL_ERROR "Graphic assets folder not found")
+    endif()
 
-	foreach (path ${pluginSourcesDir} ${guiSourcePath} ${HelpScreenSourcePath} ${IOPanelSourcePath} ${MidiControlSourcePath} ${StaffDisplaySourcePath})
-	    if (NOT EXISTS ${path})
-	        message (FATAL_ERROR "Source tree not intact - one or more child folders missing")
-	    endif()
-	endforeach()
+    foreach (path ${pluginSourcesDir} ${guiSourcePath} ${HelpScreenSourcePath} ${IOPanelSourcePath} ${MidiControlSourcePath} ${StaffDisplaySourcePath})
+        if (NOT EXISTS ${path})
+            message (FATAL_ERROR "Source tree not intact - one or more child folders missing")
+        endif()
+    endforeach()
 
-	if (IMOGEN_unitTesting AND NOT EXISTS ${testFilesPath})
-	    set (IMOGEN_unitTesting FALSE PARENT_SCOPE)
-	    message (WARNING "Test files directory not found, testing disabled")
-	endif()
+    if (IMOGEN_unitTesting AND NOT EXISTS ${testFilesPath})
+        set (IMOGEN_unitTesting FALSE PARENT_SCOPE)
+        message (WARNING "Test files directory not found, testing disabled")
+    endif()
 endfunction()
 
 ###
 
 function (checkIfCanUseExecutables)
 
-	function (_turnEmOff)
-		set (IMOGEN_launchAudioPluginHostOnBuild FALSE PARENT_SCOPE)
+    function (_turnEmOff)
+    	set (IMOGEN_launchAudioPluginHostOnBuild FALSE PARENT_SCOPE)
         set (IMOGEN_launchStandaloneOnBuild FALSE PARENT_SCOPE)
-	endfunction()
+    endfunction()
 
-	if (NOT "${CMAKE_GENERATOR}" STREQUAL "Xcode")
+    if (NOT "${CMAKE_GENERATOR}" STREQUAL "Xcode")
         message (WARNING "Auto-launching executables are currently XCode only; these have been disabled because CMake has detected that you are not generating for XCode.")
         _turnEmOff()
     elseif (NOT (APPLE OR UNIX OR WIN32))
@@ -196,14 +196,14 @@ endfunction()
 
 function (determineIfBuildingStandalone)
 
-	set (isBuildingStandalone FALSE PARENT_SCOPE)
+    set (isBuildingStandalone FALSE PARENT_SCOPE)
 
-	foreach (format ${formats})      
-	    if ("${format}" STREQUAL "Standalone")
-	        set (isBuildingStandalone TRUE PARENT_SCOPE)
-	        return()
-	    endif()
-	endforeach()
+    foreach (format ${formats})      
+        if ("${format}" STREQUAL "Standalone")
+            set (isBuildingStandalone TRUE PARENT_SCOPE)
+            return()
+        endif()
+    endforeach()
 endfunction()
 
 # =================================================================================================================================================
