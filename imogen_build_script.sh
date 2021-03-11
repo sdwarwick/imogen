fcfc2de37a784af6e560f9d760b2a386892c6fc0
Ben Vining <ben.the.vining@gmail.com>
2021-03-11 00:59:06 -0700

    Update imogen_build_script.sh

--- imogen_build_script.sh
+++ imogen_build_script.sh
@@ -18,8 +18,11 @@
 # After executing this script, the built Imogen executables will be located at /imogen/Builds/Imogen_artefacts/Release/.
 
 
+IMOGEN_DIR="$(dirname $0)" # save the directory of the script
+
+
 # set up execute permissions
-chmod 755 ${PWD}/imogen_build_script.sh
+chmod 755 "$IMOGEN_DIR"/imogen_build_script.sh
 
 
 ###  UTILITY FUNCTIONS  ###
@@ -31,11 +34,20 @@ command_exists () {
 
 # no cmake, and you're on Windows :(
 windows_no_cmake () {
-	echo -e "\n \t \v CMake cannot be found, and I've detected you're on Windows. \n"
-	echo -e "\t Installing software from the command line on Windows is the realm of gods and demons. \n"
-	echo -e "Please manually install CMake and re-run this script."
+	printf "\n \t \v CMake cannot be found, and I've detected you're on Windows. \n"
+	printf "\t Installing software from the command line on Windows is the realm of gods and demons. \n"
+	printf "Please manually install CMake and re-run this script. \n"
 	exit 1
 }
+
+
+###
+
+# check to see if the script was invoked with the --help or -h flags
+if [ ${#@} -ne 0 ] && ( [ "${@#"--help"}" = "" ] || [ "${@#"-h"}" = "" ] ); then
+  printf "\n \t \v IMOGEN BUILD SCRIPT \n USAGE: \n \n Simply execute this script with no flags or arguments to build a default release confguration of Imogen in VST, AU and Standalone formats. \n"
+  exit 0
+fi
 
 
 ###  CMAKE INSTALLATION  ###
@@ -43,45 +55,53 @@ windows_no_cmake () {
 # if CMake can't be found, install it
 if ! command_exists cmake ; then
 	case "$OSTYPE" in
-		darwin*) echo -e "\n \t \v Installing CMake..." && brew install cmake ;;  # MacOS
+		darwin*) printf "\n \t \v Installing CMake... \n \n" && brew install cmake ;;  # MacOS
 		msys*)   windows_no_cmake ;;
-		*)       echo -e "\n \t \v Installing Cmake..." && sudo apt-get -y install cmake ;;  # Linux
+		*)       printf "\n \t \v Installing Cmake... \n \n" && sudo apt-get -y install cmake ;;  # Linux
 	esac
 fi
 
 
 # if CMake still can't be found, there must be some error
 if ! command_exists cmake ; then
-	echo -e "\n \t \v CMake could not be found, and could not be automatically installed. \n Please manually install CMake and then re-run this script."
+	printf "\n \t \v CMake could not be found, and could not be automatically installed. \n Please manually install CMake and then re-run this script. \n"
 	exit 1
 fi
 
 
 ###  THE BUILD SCRIPT  ###
 
+cd "$IMOGEN_DIR" # assume that the directory of the script is also the location of the local Imogen git clone
+
 # first, make sure the local copy of the repo is up to date
-echo -e "\n \t \v Checking for new commits to Imogen remote... \n"
+printf "\n \t \v Checking for new commits to Imogen remote... \n \n"
 git pull --recurse-submodules=yes
 
 
+set -e;  # from this point forward, any errors trigger an exit signal
+
+
 # configure CMake
-echo -e "\n \t \v Configuring CMake... \n"
+printf "\n \t \v Configuring CMake... \n \n"
 cmake -B Builds --config Release -DImogen_unitTesting=FALSE -Dbv_alwaysForceCacheInits=TRUE .
 
 
 # execute build
-echo -e "\n \t \v Building Imogen... \n"
+printf "\n \t \v Building Imogen... \n \n"
 cmake --build Builds --config Release
 
 
+set +e;  # an error in zipping shouldn't be fatal
+
+
 # zip artifacts 
-echo -e "\n \t \v Zipping artifacts... \n"
+printf "\n \t \v Zipping artifacts... \n \n"
 cd Builds
 cmake -E tar cfv Imogen_build_artefacts.zip --format=zip Imogen_artefacts
 
 
-echo -e "\n \t \v Imogen built successfully!"
-echo -e "\n \t \v Enjoy! \n"
+printf "\n \t \v Imogen built successfully!"
+printf "\n \t \v Enjoy! \n"
 
 exit 0
 
