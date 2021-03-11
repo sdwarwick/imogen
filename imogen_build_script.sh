aa019978af68867ae1497f276310287eb2773302
Ben Vining <ben.the.vining@gmail.com>
2021-03-11 15:55:10 -0700

    Update imogen_build_script.sh

--- imogen_build_script.sh
+++ imogen_build_script.sh
@@ -16,6 +16,7 @@
 IMOGEN_DIR="$(dirname $0)"; # save the directory of the script
 
 ZIPPING=0;
+INSTALLING=0;
 
 
 # set up execute permissions
@@ -37,21 +38,35 @@ windows_no_cmake () {
 	exit 1
 }
 
+enable_zip () {
+	ZIPPING=1;
+  	printf "A .zip containing all the Imogen artefacts will be created after the build is complete. \n"
+}
 
-###
+unknown_argument () {
+	printf "\v Unknown argument '$1'. For usage, run this script with --help or -h. \n"
+	exit 0;
+}
+
+
+###  CHECK FOR FLAGS & ARGUMENTS  ###
 
 # check to see if the script was invoked with the --help or -h flags
-if [ ${#@} -ne 0 ] && ( [ "${@#"--help"}" = "" ] || [ "${@#"-h"}" = "" ] ); then
-  printf "\n \t \v IMOGEN BUILD SCRIPT \n USAGE: \n \n"
-  printf "Simply execute this script with no flags or arguments to build a default release confguration of Imogen in VST, AU and Standalone formats. \n"
-  printf "Invoke this script with the --zip or -z flags to zip the build artifacts together into one .zip file upon completion of the build. \n \n"
-  exit 0
+if [[ ${#@} -ne 0 ]] && ( [[ ${@#"--help"} = "" ]] || [[ ${@#"-h"} = "" ]] ); then
+ 	printf "\n \t \v IMOGEN BUILD SCRIPT \n USAGE: \n \n"
+  	printf "Simply execute this script with no flags or arguments to build a default release confguration of Imogen in VST, AU and Standalone formats. \n"
+  	printf "Invoke this script with the --zip or -z flags to zip the build artifacts together into one .zip file upon completion of the build. \n"
+  	exit 0;
 fi
 
-# check to see if the script was invoked with the --zip or -z flags
-if [[ "${@#"--zip"}" = "" ]] || [[ "${@#"-z"}" = "" ]] ; then
-  ZIPPING=1;
-fi
+
+for flag in "$@" ; do
+	case "$flag" in
+		--zip*) enable_zip ;;
+		-z*)	enable_zip ;;
+		*)		unknown_argument "$flag" ;;
+	esac
+done
 
 
 ###  CMAKE INSTALLATION  ###
@@ -100,14 +115,14 @@ if [ "$ZIPPING" -eq "1" ] ; then
 	set +e;  # an error in zipping shouldn't be fatal
 	printf "\n \t \v Zipping artifacts... \n \n"
 	cd Builds
-	cmake -E tar cfv Imogen_build_artefacts.zip --format=zip Imogen_artefacts
+	cmake -E tar cfv Imogen.zip --format=zip Imogen_artefacts/
 fi
 
 
 printf "\n \t \v Imogen built successfully!"
 printf "\n \t \v Enjoy! \n"
 
-exit 0
+exit 0;
 
 ###
 
