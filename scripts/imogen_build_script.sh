7c8495fe9827d0136551c689ae56d0130138b643
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 13:42:35 -0700

    Update imogen_build_script.sh

--- scripts/imogen_build_script.sh
+++ scripts/imogen_build_script.sh
@@ -23,9 +23,6 @@
 SCRIPT_DIR="$(dirname $0)"; # save the directory of the script
 IMOGEN_DIR="$SCRIPT_DIR/..";
 
-ZIPPING=0;
-TESTING=0;
-
 
 ###  UTILITY FUNCTIONS  ###
 
@@ -42,10 +39,6 @@ windows_no_cmake () {
 	exit 1
 }
 
-enable_zip () {
-	ZIPPING=1;
-  	printf "A .zip containing all the Imogen artefacts will be created after the build is complete. \n"
-}
 
 unknown_argument () {
 	printf "\v Unknown argument '$1'. For usage, run this script with --help or -h. \n"
@@ -59,16 +52,13 @@ unknown_argument () {
 if [[ ${#@} -ne 0 ]] && ( [[ ${@#"--help"} = "" ]] || [[ ${@#"-h"} = "" ]] ); then
  	printf "\n \t \v IMOGEN BUILD SCRIPT \n USAGE: \n \n"
   	printf "Simply execute this script with no flags or arguments to build a default release confguration of Imogen in VST, AU and Standalone formats. \n"
-  	printf "Invoke this script with the --zip or -z flags to zip the build artifacts together into one .zip file upon completion of the build. \n"
   	exit 0;
 fi
 
 
 for flag in "$@" ; do
 	case "$flag" in
-		--zip*)  enable_zip ;;
-		-z*)	 enable_zip ;;
-		*)		 unknown_argument "$flag" ;;
+		*) unknown_argument "$flag" ;;
 	esac
 done
 
@@ -112,15 +102,6 @@ cmake -DCMAKE_BUILD_TYPE=release -B Builds -DImogen_unitTesting=FALSE -Dbv_alway
 # execute build
 printf "\n \t \v Building Imogen... \n \n"
 cmake --build Builds --target Imogen_All --config Release
-
-
-# zip artifacts (if requested by user)
-if [ "$ZIPPING" -eq "1" ] ; then
-	set +e;  # an error in zipping shouldn't be fatal
-	printf "\n \t \v Zipping artifacts... \n \n"
-	cd Builds
-	cmake -E tar cfv Imogen.zip --format=zip Imogen_artefacts/
-fi
 
 
 printf "\n \t \v Imogen built successfully!"
