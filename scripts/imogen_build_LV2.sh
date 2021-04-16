0d1f5bf481efb99ccfa65b5d1f52d0c3f477bf73
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 16:36:06 -0700

    Update imogen_build_LV2.sh

--- scripts/imogen_build_LV2.sh
+++ scripts/imogen_build_LV2.sh
@@ -95,7 +95,7 @@ set -e;  # from this point forward, any errors trigger an exit signal
 
 # configure CMake
 printf "\n \t \v Configuring CMake... \n \n"
-cmake -DCMAKE_BUILD_TYPE=release -B Builds/LV2_build -DImogen_unitTesting=FALSE -Dbv_alwaysForceCacheInits=TRUE -Dbv_formats=LV2 -Dbv_juceGitRepoToUse="https://github.com/lv2-porting-project/JUCE.git" .
+cmake -DCMAKE_BUILD_TYPE=release -B Builds/LV2_build -DImogen_unitTesting=FALSE -Dbv_alwaysForceCacheInits=TRUE -Dbv_formats=LV2 -Dbv_juceGitRepoToUse="https://github.com/DISTRHO/JUCE" .
 
 
 # execute build
