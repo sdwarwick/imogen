1a8615598bd1515ddff8040e30abde1fb2e5b0d9
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 01:06:52 -0700

    Update StandaloneWrapper.h

--- Source/StandaloneWrapper/StandaloneWrapper.h
+++ Source/StandaloneWrapper/StandaloneWrapper.h
@@ -28,3 +28,25 @@ private:
 };
 
 
+struct PropertiesFileOptions : public PropertiesFile::Options
+{
+    PropertiesFileOptions()
+    {
+        applicationName = JucePlugin_Name;
+        filenameSuffix = ".settings";
+        osxLibrarySubFolder = "Application Support";
+        folderName = getOptionsFolderName();
+    }
+
+    static String getOptionsFolderName()
+    {
+#if JUCE_LINUX
+        return "~/.config";
+#else
+        return "";
+#endif
+    }
+};
+
+
+
