06096b1650e16fb15fac89415a724eb843c27a38
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 00:46:57 -0700

    Create StandaloneWrapper.cpp

---
+++ Source/StandaloneWrapper/StandaloneWrapper.cpp
@@ -0,0 +1,58 @@
+#include "StandaloneWrapper.h"
+#include "ConfigurationHelpers.h"
+
+StandaloneFilterApp::StandaloneFilterApp()
+{
+    juce::PluginHostType::jucePlugInClientCurrentWrapperType = juce::AudioProcessor::wrapperType_Standalone;
+
+    appProperties.setStorageParameters (PropertiesFileOptions());
+}
+
+juce::StandaloneFilterWindow* StandaloneFilterApp::createWindow()
+{
+    return new juce:: StandaloneFilterWindow (getApplicationName(),
+                                              Helpers::getBackgroundColor(),
+                                              appProperties.getUserSettings(),
+                                              false,
+                                              {},
+                                              nullptr,
+                                              Helpers::getChannelConfigurations(),
+                                              Helpers::shouldAutoOpenMidiDevices());
+}
+
+void StandaloneFilterApp::initialise (const juce::String&)
+{
+    mainWindow.reset (createWindow());
+
+    if (! Helpers::shouldUseKioskMode())
+        mainWindow->setVisible (true);
+}
+
+void StandaloneFilterApp::shutdown()
+{
+    mainWindow = nullptr;
+    appProperties.saveIfNeeded();
+}
+
+void StandaloneFilterApp::systemRequestedQuit()
+{
+    if (mainWindow != nullptr)
+        mainWindow->pluginHolder->savePluginState();
+
+    if (juce::ModalComponentManager::getInstance()->cancelAllModalComponents())
+        juce::Timer::callAfterDelay(100, [&]() { requestQuit(); });
+    else
+        quit();
+}
+
+void StandaloneFilterApp::requestQuit() const
+{
+    if (auto app = getInstance())
+        app->systemRequestedQuit();
+}
+
+
+juce::JUCEApplicationBase* juce_CreateApplication()
+{
+    return new StandaloneFilterApp();
+}
