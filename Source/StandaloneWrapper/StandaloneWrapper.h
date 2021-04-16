61be417fd3154c502dfc7f2bc43646a4fb1b70ad
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 11:20:36 -0700

    Update StandaloneWrapper.h

--- Source/StandaloneWrapper/StandaloneWrapper.h
+++ Source/StandaloneWrapper/StandaloneWrapper.h
@@ -5,6 +5,8 @@
 #include <juce_audio_plugin_client/juce_audio_plugin_client.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_audio_plugin_client/Standalone/juce_StandaloneFilterWindow.h>
+#include <juce_data_structures/app_properties/juce_PropertiesFile.h>
+
 
 extern juce::JUCEApplicationBase* juce_CreateApplication();
 
