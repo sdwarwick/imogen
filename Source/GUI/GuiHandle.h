6d66f9925ecdd343407bfe5a94f6bc53c778bc9c
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 23:46:30 -0700

    Create GuiHandle.h

---
+++ Source/GUI/GuiHandle.h
@@ -0,0 +1,35 @@
+/*======================================================================================================================================================
+           _             _   _                _                _                 _               _
+          /\ \          /\_\/\_\ _           /\ \             /\ \              /\ \            /\ \     _
+          \ \ \        / / / / //\_\        /  \ \           /  \ \            /  \ \          /  \ \   /\_\
+          /\ \_\      /\ \/ \ \/ / /       / /\ \ \         / /\ \_\          / /\ \ \        / /\ \ \_/ / /
+         / /\/_/     /  \____\__/ /       / / /\ \ \       / / /\/_/         / / /\ \_\      / / /\ \___/ /
+        / / /       / /\/________/       / / /  \ \_\     / / / ______      / /_/_ \/_/     / / /  \/____/
+       / / /       / / /\/_// / /       / / /   / / /    / / / /\_____\    / /____/\       / / /    / / /
+      / / /       / / /    / / /       / / /   / / /    / / /  \/____ /   / /\____\/      / / /    / / /
+  ___/ / /__     / / /    / / /       / / /___/ / /    / / /_____/ / /   / / /______     / / /    / / /
+ /\__\/_/___\    \/_/    / / /       / / /____\/ /    / / /______\/ /   / / /_______\   / / /    / / /
+ \/_________/            \/_/        \/_________/     \/___________/    \/__________/   \/_/     \/_/
+ 
+ 
+ This file is part of the Imogen codebase.
+ 
+ @2021 by Ben Vining. All rights reserved.
+ 
+ GuiHandle.h: A GuiHandle object is essentially the relay between an ImogenGui object and whatever it is controlling. For a normal plugin or standalone, the GuiHandle will just connect the parameters to the processor's parameters, but this class is abstract so that it can be customized, for example, to create a GUI-only remote control.
+ 
+======================================================================================================================================================*/
+
+#pragma once
+
+class ImogenGuiHandle
+{
+public:
+    ImogenGuiHandle() = default;
+  
+    virtual ~ImogenGuiHandle() = default;
+  
+    virtual void loadPreset (juce::String presetName)=0;
+  
+private:
+};
