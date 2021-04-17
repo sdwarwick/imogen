4345e291fa2b075c0bb782f0df97836e7243ec48
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-16 22:23:04 -0700

    Update ImogenGUI.h

--- Source/GUI/ImogenGUI.h
+++ Source/GUI/ImogenGUI.h
@@ -1,1 +1,56 @@
+/*======================================================================================================================================================
+           _             _   _                _                _                 _               _
+          /\ \          /\_\/\_\ _           /\ \             /\ \              /\ \            /\ \     _
+          \ \ \        / / / / //\_\        /  \ \           /  \ \            /  \ \          /  \ \   /\_\
+          /\ \_\      /\ \/ \ \/ / /       / /\ \ \         / /\ \_\          / /\ \ \        / /\ \ \_/ / /
+         / /\/_/     /  \____\__/ /       / / /\ \ \       / / /\/_/         / / /\ \_\      / / /\ \___/ /
+        / / /       / /\/________/       / / /  \ \_\     / / / ______      / /_/_ \/_/     / / /  \/____/
+       / / /       / / /\/_// / /       / / /   / / /    / / / /\_____\    / /____/\       / / /    / / /
+      / / /       / / /    / / /       / / /   / / /    / / /  \/____ /   / /\____\/      / / /    / / /
+  ___/ / /__     / / /    / / /       / / /___/ / /    / / /_____/ / /   / / /______     / / /    / / /
+ /\__\/_/___\    \/_/    / / /       / / /____\/ /    / / /______\/ /   / / /_______\   / / /    / / /
+ \/_________/            \/_/        \/_________/     \/___________/    \/__________/   \/_/     \/_/
+ 
+ 
+ This file is part of the Imogen codebase.
+ 
+ @2021 by Ben Vining. All rights reserved.
+ 
+ ImogenGUI.h: This file defines the interface for Imogen's top-level GUI component. This class does not reference ImogenAudioProcessor, so that it can also be used to  create a GUI-only remote control application for Imogen.
+ 
+======================================================================================================================================================*/
 
+
+#pragma once
+
+#include <juce_gui_extra/juce_gui_extra.h>
+#include "GUI/LookAndFeel/ImogenLookAndFeel.h"
+
+
+class ImogenGUI  :     public juce::Component,
+                       public juce::Timer
+{
+public:
+    
+    ImogenGUI();
+    
+    virtual ~ImogenGUI() override;
+    
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+    
+    void timerCallback() override;
+    
+    
+private:
+    
+    inline void makePresetMenu (juce::ComboBox& box);
+    
+    void updateParameterDefaults();
+    
+    juce::ComboBox selectPreset;
+    
+    bav::ImogenLookAndFeel lookAndFeel;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ImogenGUI)
+};
