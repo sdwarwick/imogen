1519388c8f90f07a9e5015cc47ee4a46a59f92a4
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-27 23:08:39 -0700

    Create imogen_install_Linux_deps.sh

---
+++ scripts/imogen_install_Linux_deps.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#            _             _   _                _                _                 _               _
+#           /\ \          /\_\/\_\ _           /\ \             /\ \              /\ \            /\ \     _
+#           \ \ \        / / / / //\_\        /  \ \           /  \ \            /  \ \          /  \ \   /\_\
+#           /\ \_\      /\ \/ \ \/ / /       / /\ \ \         / /\ \_\          / /\ \ \        / /\ \ \_/ / /
+#          / /\/_/     /  \____\__/ /       / / /\ \ \       / / /\/_/         / / /\ \_\      / / /\ \___/ /
+#         / / /       / /\/________/       / / /  \ \_\     / / / ______      / /_/_ \/_/     / / /  \/____/
+#        / / /       / / /\/_// / /       / / /   / / /    / / / /\_____\    / /____/\       / / /    / / /
+#       / / /       / / /    / / /       / / /   / / /    / / /  \/____ /   / /\____\/      / / /    / / /
+#   ___/ / /__     / / /    / / /       / / /___/ / /    / / /_____/ / /   / / /______     / / /    / / /
+#  /\__\/_/___\    \/_/    / / /       / / /____\/ /    / / /______\/ /   / / /_______\   / / /    / / /
+#  \/_________/            \/_/        \/_________/     \/___________/    \/__________/   \/_/     \/_/
+ 
+ 
+#  This file is part of the Imogen codebase.
+ 
+#  @2021 by Ben Vining. All rights reserved.
+
+#  imogen_build_iOS.sh :	  This script builds an iOS version of Imogen. When cross compiling for iOS, using MacOS is recommended, as is using CMake's XCode generator.
+
+
+sudo apt-get update;
+
+sudo apt-get install -y libasound2-dev libx11-dev libxinerama-dev libxext-dev libfreetype6-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev libxcursor-dev libxrandr-dev
+
