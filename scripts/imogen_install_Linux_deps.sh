6870bfe3934b79ec4461b529c71d34207544de07
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-04-27 23:12:15 -0700

    Update imogen_install_Linux_deps.sh

--- scripts/imogen_install_Linux_deps.sh
+++ scripts/imogen_install_Linux_deps.sh
@@ -22,5 +22,5 @@
 
 sudo apt-get update;
 
-sudo apt-get install -y libasound2-dev libx11-dev libxinerama-dev libxext-dev libfreetype6-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev libxcursor-dev libxrandr-dev
+sudo apt-get install -y libasound2-dev libx11-dev libxinerama-dev libxext-dev libfreetype6-dev libwebkit2gtk-4.0-dev libglu1-mesa-dev libxcursor-dev libxrandr-dev libgtk-3-dev
 
