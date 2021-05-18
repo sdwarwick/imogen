dde3c3307a943c4ddc2414b0f1896b0c428e7b3a
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-18 03:34:48 -0700

    updating

--- CMake/FindShared-code.cmake
+++ CMake/FindShared-code.cmake
@@ -3,4 +3,4 @@ include (${CMAKE_CURRENT_LIST_DIR}/CPM.cmake)
 CPMAddPackage(
         NAME Shared-code
         GIT_REPOSITORY https://github.com/benthevining/Shared-code.git
-        GIT_TAG origin/master)
+        GIT_TAG origin/main)
