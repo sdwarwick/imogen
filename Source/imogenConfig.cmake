24c3f5dbd32d519089c3636eacb4175c64dbe42b
Ben Vining <73820785+benthevining@users.noreply.github.com>
2021-05-18 03:13:23 -0700

    updating

--- Source/imogenConfig.cmake
+++ Source/imogenConfig.cmake
@@ -1,4 +1,36 @@
 
+#
+
+if (DEFINED bv_ignoreAllThirdPartyLibs)
+    set (bv_use_AbletonLink FALSE)
+    set (bv_use_MTS_ESP FALSE)
+else()
+    if (NOT DEFINED bv_use_AbletonLink)
+       set (bv_use_AbletonLink TRUE)
+    endif()
+
+    if (NOT DEFINED bv_use_MTS_ESP)
+       set (bv_use_MTS_ESP TRUE)
+    endif()
+endif()
+
+#
+
+if (DEFINED bv_formats)
+    set (lv2detector -1)
+    list (FIND bv_formats LV2 lv2detector)
+
+    if (NOT ${lv2detector} EQUAL -1)
+        set_source_files_properties (${bv_juceDir}/extras/Build/lv2_ttl_generator/lv2_ttl_generator.c PROPERTIES LANGUAGE CXX)
+        set (bv_use_AbletonLink FALSE)
+    endif()
+else()
+    set (bv_formats AU VST3 Standalone Unity)
+endif()
+
+#
+
+
 set (ImogenIconPath ${Imogen_sourceDir}/../assets/graphics/imogen_icon.png)
 
 set (Imogen_Common_SourceFiles
@@ -19,12 +51,6 @@ set (Imogen_GUI_SourceFiles
     ${Imogen_sourceDir}/GUI/MainDialComponent/MainDialComponent.cpp
     ${Imogen_sourceDir}/GUI/LookAndFeel/ImogenLookAndFeel.h
     ${Imogen_sourceDir}/GUI/LookAndFeel/ImogenLookAndFeel.cpp)
-
-#
-
-set (Imogen_assetFiles
-	${ImogenIconPath}
-	)
 
 #
 
@@ -64,103 +90,9 @@ set (Imogen_Plugin_Flags
     MICROPHONE_PERMISSION_TEXT      "Imogen requires audio input to be able to produce its output. Please enable the microphone, or you won't hear anything when you press the keys."
     )
 
-#
-
-set_property (GLOBAL PROPERTY USE_FOLDERS YES)
-set_property (GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER "Build Targets")
-
-if (APPLE)
-	set (CMAKE_OSX_ARCHITECTURES "x86_64;arm64" CACHE INTERNAL "")  # universal macOS binaries 
-	set (CMAKE_OSX_DEPLOYMENT_TARGET "10.11" CACHE STRING "Minimum OS X deployment version" FORCE)  # minimum MacOS version to build for
-elseif (WIN32)
-	set (CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")  # static linking on Windows
-	set (CMAKE_WIN32_EXECUTABLE TRUE)
-endif()
-
-set (CMAKE_XCODE_GENERATE_SCHEME OFF)  # schemes are maually generated for each target to avoid clutter from modules getting schemes, etc
-
-set (CMAKE_SUPPRESS_REGENERATION TRUE)  # no "zero-check" target
-set (CMAKE_OPTIMIZE_DEPENDENCIES TRUE)
-set (CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
-
-option (JUCE_ENABLE_MODULE_SOURCE_GROUPS "Enable Module Source Groups" ON)
-option (JUCE_BUILD_EXAMPLES "Build JUCE Examples" OFF)
-option (JUCE_BUILD_EXTRAS "Build JUCE Extras" OFF)
-
-#
-
-if (ANDROID)
-    add_definitions (
-    "-DJUCE_ANDROID=1" 
-    "-DJUCE_PUSH_NOTIFICATIONS=1" 
-    "-DJUCE_PUSH_NOTIFICATIONS_ACTIVITY=\"com/rmsl/juce/JuceActivity\"" 
-    )
-endif()
-
-#
-
-# ADD JUCE #
-
-if (DEFINED bv_juceDir)
-	add_subdirectory (${bv_juceDir} ${CMAKE_CURRENT_SOURCE_DIR}/Builds/JUCE)
-else()
-	if (NOT DEFINED bv_juceGitRepoToUse)
-        set (bv_juceGitRepoToUse https://github.com/juce-framework/JUCE.git)
-	endif()
-
-	if (NOT DEFINED bv_juceGitRepo_TagToUse)
-        if ("${bv_juceGitRepoToUse}" STREQUAL "https://github.com/juce-framework/JUCE.git")
-	        set (bv_juceGitRepo_TagToUse origin/develop)
-		else()
-	        set (bv_juceGitRepo_TagToUse origin)
-		endif()
-	endif()
-
-	message (STATUS "Fetching the latest version of JUCE from GitHub repo: ${bv_juceGitRepoToUse} with tag: ${bv_juceGitRepo_TagToUse} ")
-
-	include (FetchContent)
-
-	FetchContent_Declare (juce
-	GIT_REPOSITORY ${bv_juceGitRepoToUse}
-	GIT_TAG        ${bv_juceGitRepo_TagToUse})
-
-	FetchContent_MakeAvailable (juce)
-
-	set (bv_juceDir ${CMAKE_CURRENT_LIST_DIR}/Builds/_deps/juce-src)
-endif()
-
-#
-
-if (ANDROID)
-    set (OBOE_DIR "../JUCE/modules/juce_audio_devices/native/oboe")
-    add_subdirectory (${OBOE_DIR} ./oboe)
-
-    add_library ("cpufeatures" STATIC "${ANDROID_NDK}/sources/android/cpufeatures/cpu-features.c")
-    set_source_files_properties ("${ANDROID_NDK}/sources/android/cpufeatures/cpu-features.c" PROPERTIES COMPILE_FLAGS "-Wno-sign-conversion -Wno-gnu-statement-expression")
-
-    enable_language (ASM)
-endif()
-
 ##################################################################################################
 
 function (imogenConfig_step2)
-
-if (ANDROID)
-    find_library (log "log")
-    find_library (android "android")
-    find_library (glesv2 "GLESv2")
-    find_library (egl "EGL")
-    set (cpufeatures_lib "cpufeatures")
-    set (oboe_lib "oboe")
-
-    target_include_directories (${CMAKE_PROJECT_NAME} PRIVATE
-        "${ANDROID_NDK}/sources/android/cpufeatures"
-        "${OBOE_DIR}/include")
-
-    target_link_libraries (${CMAKE_PROJECT_NAME} PUBLIC ${log} ${android} ${glesv2} ${egl} ${cpufeatures_lib} ${oboe_lib})
-endif()
-
-#
 
 if (DEFINED BV_IGNORE_VDSP OR NOT APPLE)
 
@@ -179,19 +111,9 @@ endif()
 
 #
 
-target_link_libraries (${CMAKE_PROJECT_NAME} PUBLIC
-	    juce::juce_recommended_config_flags
-	    juce::juce_recommended_lto_flags
-	    juce::juce_recommended_warning_flags
-)
-
 target_compile_features (${CMAKE_PROJECT_NAME} PUBLIC cxx_std_17)
 
 target_compile_definitions (${CMAKE_PROJECT_NAME} PUBLIC 
-    JUCE_STRICT_REFCOUNTEDPTR=1
-    JUCE_VST3_CAN_REPLACE_VST2=0
-    JUCE_MODAL_LOOPS_PERMITTED=0
-    JUCE_LOAD_CURL_SYMBOLS_LAZILY=1
     BV_HAS_BINARY_DATA=1
     )
 
