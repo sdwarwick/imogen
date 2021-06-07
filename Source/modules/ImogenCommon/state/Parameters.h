320fc3cad0c0a892d551612cf187129479918863
Ben Vining <ben.the.vining@gmail.com>
2021-06-07 00:22:51 -0700

    Update Parameters.h

--- Source/modules/ImogenCommon/state/Parameters.h
+++ Source/modules/ImogenCommon/state/Parameters.h
@@ -14,6 +14,8 @@ struct Parameters : ParameterList
         add (inputMode, dryWet, inputGain, outputGain, mainBypass, leadBypass, harmonyBypass, stereoWidth, lowestPanned, leadPan, pitchbendRange, velocitySens, aftertouchToggle, voiceStealing, pedalToggle, pedalThresh, descantToggle, descantThresh, descantInterval, adsrAttack, adsrDecay, adsrSustain, adsrRelease, noiseGateToggle, noiseGateThresh, deEsserToggle, deEsserThresh, deEsserAmount, compToggle, compAmount, delayToggle, delayDryWet, reverbToggle, reverbDryWet, reverbDecay, reverbDuck, reverbLoCut, reverbHiCut, limiterToggle, midiLatch);
 
         addInternal (editorPitchbend);
+        
+        setPitchbendParameter (editorPitchbend);
     }
 
     IntParam inputMode {"Input source", "Input source", 1, 3, 1,
