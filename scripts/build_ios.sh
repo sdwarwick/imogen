c91b5fe2381486c8b72a1e42adbf392b8174b3a3
Ben Vining <ben.the.vining@gmail.com>
2021-07-11 01:19:17 -0700

    Create build_ios.sh

---
+++ scripts/build_ios.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR/..
+
+cmake -Bbuild-ios -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=9.3
+
+cmake --build build-ios -- -allowProvisioningUpdates -sdk iphonesimulator
