202f2c525ba330ae599b705e3194bcae0e9df642
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 04:12:56 -0700

    Update build_LV2.sh

--- scripts/build_LV2.sh
+++ scripts/build_LV2.sh
@@ -13,9 +13,10 @@ mkdir JUCE
 cd JUCE
 
 git clone https://github.com/lv2-porting-project/JUCE.git
+JUCE_DIR=$PWD
 
 cd $SCRIPT_DIR/..
 
-cmake -B Builds -DFormats=LV2 -DCPM_JUCE_SOURCE=$SCRIPT_DIR/Builds/JUCE
+cmake -B Builds -DFormats=LV2 -DCPM_JUCE_SOURCE=$JUCE_DIR
 
 cmake --build Builds --target Imogen_LV2
