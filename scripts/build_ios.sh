7ea076e2bfac686c7fe681a0889bd508c1562a5b
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 10:01:30 -0700

    updating

--- scripts/build_ios.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cd $SCRIPT_DIR/..
-
-cmake -Bbuild-ios -GXcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=9.3
-
-cmake --build build-ios --target Imogen_Standalone -- -allowProvisioningUpdates -sdk iphonesimulator
