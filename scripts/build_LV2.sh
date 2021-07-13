0672577f6619a3127abc33321cb77fed0a62e8b9
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 05:13:23 -0700

    Update build_LV2.sh

--- scripts/build_LV2.sh
+++ scripts/build_LV2.sh
@@ -4,6 +4,8 @@ sudo apt-get install lv2-dev
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+bash $SCRIPT_DIR/install_linux_deps.sh
+
 cd $SCRIPT_DIR/..
 
 git clone -b lv2 https://github.com/lv2-porting-project/JUCE.git
@@ -18,4 +20,4 @@ cd $SCRIPT_DIR/..
 
 cmake -B Builds -DFormats=LV2 -DCPM_JUCE_SOURCE=$JUCE_DIR
 
-cmake --build Builds --target Imogen_LV2
+cmake --build Builds
