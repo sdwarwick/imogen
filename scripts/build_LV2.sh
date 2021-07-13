5ab43f14637df0e10d39c6550d9c53de245b7285
Ben Vining <ben.the.vining@gmail.com>
2021-07-13 09:37:07 -0700

    Update build_LV2.sh

--- scripts/build_LV2.sh
+++ scripts/build_LV2.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+	printf ("LV2 is only available on Linux :(")
+	exit 0;
+fi
+
 sudo apt-get install lv2-dev
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-bash $SCRIPT_DIR/install_linux_deps.sh
+bash $SCRIPT_DIR/install_juce_linux_deps.sh
 
-cd $SCRIPT_DIR/..
+if [[ $# > 0 ]]; then
+	REPO_DIR=$1
+else
+	REPO_DIR=$SCRIPT_DIR/..
+fi
+
+cd $REPO_DIR/Builds
 
 git clone -b lv2 https://github.com/lv2-porting-project/JUCE.git
 
 cd JUCE
 
 JUCE_DIR=$PWD
-
 git pull
 
-cd $SCRIPT_DIR/..
+cd $REPO_DIR
 
 cmake -B Builds -DFormats=LV2 -DCPM_JUCE_SOURCE=$JUCE_DIR
 
