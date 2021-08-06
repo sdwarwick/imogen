7b5de0187bf068f16c5d956aae6a269c9e364e6f
Ben Vining <ben.the.vining@gmail.com>
2021-08-06 04:28:34 -0700

    Update remote_main.cpp

--- Source/remote_main.cpp
+++ Source/remote_main.cpp
@@ -7,7 +7,7 @@ namespace Imogen
 struct RemoteApp : bav::GuiApp< Remote >
 {
     RemoteApp()
-        : bav::GuiApp< Imogen::Remote > (String ("Imogen ") + TRANS ("Remote"), "0.0.1")
+        : bav::GuiApp< Imogen::Remote > (String ("Imogen ") + TRANS ("Remote"), "0.0.1", {1060, 640})
     {
     }
 };
